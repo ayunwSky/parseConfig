adaf5c7e3fcd30542acf2c4b0b07ca2924ffc1d6
ayunw <allen_jol@163.com>
2022-08-15 18:06:02 +0800

    feat: viper 解析各种格式的配置文件

---
+++ main.go
@@ -0,0 +1,28 @@
+/**
+ * @Author: ayunwSky
+ * @Date: 2022/8/14 20:40
+ * @Desc:
+ */
+
+package main
+
+import (
+	"parseConfig/viperGetItem"
+	vji "parseConfig/viperJsonItem"
+	"parseConfig/viperToml"
+	"parseConfig/viperTomlAndJson"
+)
+
+func main() {
+	//fmt.Println("I amd main function...---------------------------")
+	viperToml.ParseToml()
+
+	//fmt.Println("I amd main function...---------------------------")
+	viperTomlAndJson.ParseTomlAndJson()
+
+	//fmt.Println("I amd main function...---------------------------")
+	viperGetItem.ParseItems()
+
+	//fmt.Println("I amd main function...---------------------------")
+	vji.ParseJsonItem()
+}
